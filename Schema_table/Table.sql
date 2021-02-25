830c01c13e331707f10c74614c0de97583617af9
Alphonse Fournier <a.fournier@net.estia.fr>
2021-02-25 16:43:11 +0100

    table clients

--- Schema_table/Table.sql
+++ Schema_table/Table.sql
@@ -1,48 +1,23 @@
--- CREATION TABLESPACE ET USER
+-- CREATION DES TABLES
 
-cmd
--- Lancer sqlplus sans se logger
-sqlplus /nolog
 
--- Définir la vairiable qui va contenir le nom réseau de votre base.
--- Le nom réseau se dans le fichier tnsnames.ora
--- Il est disponible dans le dossier : %ORACLE_HOME%\network\admin
-define DBALIAS=ORCLPDB1
+-- Table client : age, sexe, taux, situationFamiliale, nbEnfantsAcharge, 2eme voiture, immatriculation
 
-	
--- Définir la variable contenant le nom de l'utilisateur que vous allez créer
-
-define MYUSER=Projet_TUNING_BDD_MOMAS_TAMANINI_FOURNIER
-
--- suprimer l'utilisateur s'il existe déjà
-
-drop user &MYUSER cascade;
-
--- Création de l'utilisateur. 
-
-create user &MYUSER identified by PassOrs1
-default tablespace Projet_TUNING
-temporary tablespace temp;
-
--- affecter et enlever des droits
-grant dba to &MYUSER;
-
-revoke unlimited tablespace from &MYUSER;
-
--- Définir la variable contenant le password du compte SYSTEM
-define SYSTEMPASSWORD=Dbamanager1
-
--- Définir la variable
-define MYINSTANCE=orcl
-
--- Création du TableSpace
-
-connect sys as dba
-
-CREATE TABLESPACE Projet_TUNING DATAFILE '%ORACLE_BASE%\oradata\&MYINSTANCE\&DBALIAS\Projet_TUNING_01.DBF' SIZE 200M AUTOEXTEND on next 50M
-    EXTENT MANAGEMENT LOCAL AUTOALLOCATE SEGMENT SPACE MANAGEMENT AUTO;
-
-	
--- Se connecter avec votre compte System pour créer l'utilisateur 
-connect system@&DBALIAS/&SYSTEMPASSWORD
+Create table Clients(
+	clientId NUMBER GENERATED ALWAYS AS IDENTITY constraint pk_clientId primary key,
+	age	number(2) constraint chk_age_client
+		check(age BETWEEN 18 AND 84),
+	sexe varchar2(30) constraint chk_sexe 
+		check(sexe IN ('M', 'F')),
+	taux number(5) constraint chk_taux
+		check(age BETWEEN 544 AND 74185),
+	situationFamiliale varchar2(30)constraint chk_situation
+		check(sexe IN ('Célibataire', 'Divorcée', 'En Couple', 'EnCouple', 'Marié', 'Mariée', 'Seul', 'Seule')),
+	nbEnfantsAcharge number(2) constraint chk_nb_enfants
+		check(nbEnfantsAcharge BETWEEN 0 AND 4),
+	deuxiemeVoiture varchar2(10) constraint chk_voiture 
+		check(deuxiemeVoiture IN ('true', 'false')),
+	immatriculation varchar2(10) constraint chk_immat
+		CHECK (LENGTHB(immatriculation) == 10)
+) ;
 
