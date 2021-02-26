9d97f13201453040df9f7eaeeb0dd0c3e83db889
Alphonse Fournier <a.fournier@net.estia.fr>
2021-02-26 09:15:17 +0100

    last version

--- 04_Insertion_lignes/4.1_Table_catalogue/import_table_catalogue.sql
+++ 04_Insertion_lignes/4.1_Table_catalogue/import_table_catalogue.sql
@@ -5,3 +5,5 @@ INFILE 'C:\GitHub\Projet_TUNING_BDD_MOMAS_TAMANIN_FOURNIER\Data\Catalogue.csv'
 INTO TABLE Catalogue
 FIELDS TERMINATED BY ','
 (marque, nom, puissance, longueur, nbPlaces, nbPortes, couleur, occasion, prix)
+
+
