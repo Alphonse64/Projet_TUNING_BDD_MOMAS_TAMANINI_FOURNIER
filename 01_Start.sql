f7e31bab991da10f44a5fb728b51443b17b53a1a
Lisa <l.momas@net.estia.fr>
2021-02-26 09:57:14 +0100

    maj define lisa

--- 01_Start.sql
+++ 01_Start.sql
@@ -3,13 +3,28 @@ cmd
 -- Lancer sqlplus sans se logger
 sqlplus /nolog
 
+
+--Lisa
+define MYINSTANCE=PBEST21
+define DBALIASPDB=pdbest21
+define DBALIASCDB=pbest21
+define MYCDBUSER=SYSTEM
+define MYCDBUSERPASS=DBAManager1
+define MYPDBUSER=Projet_TUNING_BDD_MOMAS_TAMANINI_FOURNIER
+define MYPDBUSERPASS=PassOrs2
+
 -- Définir la variable contenant le nom de l'instance
 define MYINSTANCE=orcl
+
+
 
 -- Définir la vairiable qui va contenir le nom réseau de votre base PDB.
 -- Le nom réseau se trouve dans le fichier tnsnames.ora
 -- Il est disponible dans le dossier : %ORACLE_HOME%\network\admin
 define DBALIASPDB=ORCLPDB1
+
+
+
 
 -- Définir la vairiable qui va contenir le nom réseau de votre base CDB.
 -- Le nom réseau se dans le fichier tnsnames.ora
