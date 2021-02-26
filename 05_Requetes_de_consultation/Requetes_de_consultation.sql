23d2bee364f33661d372dfa80e56ca9d3f01d69c
Alphonse Fournier <a.fournier@net.estia.fr>
2021-02-26 14:34:09 +0100

    requetes

--- 05_Requetes_de_consultation/Requetes_de_consultation.sql
+++ 05_Requetes_de_consultation/Requetes_de_consultation.sql
@@ -83,17 +83,76 @@ WHERE immatriculation.puissance > 150
 ORDER BY client.age DESC;
 
 
--- Voir l'age des clients et la marque des voitures prise pour les voitures puissates (>150)
+-- Voir la marque des voitures avec une puissante >150
 SELECT DISTINCT immatriculation.puissance, immatriculation.marque
 FROM immatriculation
 JOIN catalogue 
 ON immatriculation.marque = catalogue.marque
 JOIN client 
 ON immatriculation.immatriculation = client.immatriculation
-WHERE immatriculation.puissance > 150
-ORDER BY client.age DESC;
-
+WHERE immatriculation.puissance > 150 
+ORDER BY immatriculation.puissance  DESC;
 
 
 -- REQUETES PLUS DE 3 TABLES
+
+--Selectionner l'age de tous les conducteurs de Dacia.
+SELECT DISTINCT marketing.age
+FROM marketing
+JOIN client
+ON client.sexe = marketing.sexe
+JOIN immatriculation
+ON client.immatriculation=immatriculation.immatriculation 
+JOIN catalogue
+ON catalogue.puissance=immatriculation.puissance
+WHERE immatriculation.marque='Dacia'
+ORDER BY marketing.age DESC;
+
+--Couleur de la voiture de tous les clients de la table marketing qui ont moins de 2 enfants.
+SELECT DISTINCT catalogue.couleur
+FROM catalogue
+JOIN immatriculation
+ON catalogue.puissance=immatriculation.puissance
+JOIN client
+ON client.immatriculation=immatriculation.immatriculation
+JOIN marketing
+ON client.sexe = marketing.sexe
+WHERE marketing.nbEnfantsAcharge<2;
+
+
+--Afficher l'id, le taux et la situation familiale des clients qui possèdent une Dacia.
+SELECT DISTINCT  marketing.clientMarketingId, marketing.taux, marketing.situationFamiliale
+FROM marketing
+JOIN client
+ON client.sexe = marketing.sexe
+JOIN immatriculation
+ON client.immatriculation=immatriculation.immatriculation 
+JOIN catalogue
+ON catalogue.puissance=immatriculation.puissance
+WHERE immatriculation.marque='Dacia'
+ORDER BY marketing.clientMarketingId DESC;
+
+-- Selectionner la marque de voiture que prend les clients qui ont comme situation familiale "En Couple".
+SELECT DISTINCT immatriculation.marque
+FROM immatriculation
+JOIN catalogue
+ON immatriculation.marque = catalogue.marque
+JOIN client
+ON immatriculation.immatriculation = client.immatriculation
+JOIN marketing
+ON client.age = marketing.age
+WHERE marketing.situationFamiliale = 'En Couple';
+
+--Afficher l'id du client, la marque et le prix du vehicule des clients de sexe féminin de la table marketing.
+SELECT DISTINCT client.clientId, catalogue.marque, catalogue.prix
+FROM catalogue
+JOIN immatriculation
+ON catalogue.puissance=immatriculation.puissance
+JOIN client
+ON client.immatriculation=immatriculation.immatriculation
+JOIN marketing
+ON client.sexe = marketing.sexe
+WHERE marketing.sexe='F';
+
+
 
